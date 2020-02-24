0fd5b39d6a1b93bd292d365b05bc918688b61f02
sophmintaii <sophmintaii@gmail.com>
2020-02-24 22:45:10 +0200

    Deleted keys

--- hidden.py
+++ hidden.py
@@ -3,8 +3,10 @@
 # https://apps.twitter.com/
 # Create new App and get the four strings
 
+
+#put your API keys here
 def oauth():
-    return {"consumer_key": "0PlKlKMbNYWL9UeVuC6DPxX4B",
-            "consumer_secret": "dpvpCGjA9udpjsPkCVfdxZhmAOLMXFeZcZEVLbH3usRiv8fHfN",
-            "token_key": "963011359737774080-mYiiiIGOarKbuYGVdVTJBPcYC39RkqC",
-            "token_secret": "f8031GVQo6dJXX35RRVbDlGxMcitrAt7uyAUVgKuO1E0R"}
+    return {"consumer_key": "",
+            "consumer_secret": "",
+            "token_key": "",
+            "token_secret": ""}
